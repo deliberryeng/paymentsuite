2c9dbaf22b5c0b9d9b11b8c08607dbbed8e569dc
marctomasdeliberry <marc.tomas@deliberry.com>
2016-06-28 15:23:08 +0200

    [DEV-773] Added repository function (#7)

--- src/PaymentSuite/RedsysApiBundle/Repository/TransactionRepository.php
+++ src/PaymentSuite/RedsysApiBundle/Repository/TransactionRepository.php
@@ -19,5 +19,22 @@ use Doctrine\ORM\EntityRepository;
 
 class TransactionRepository extends EntityRepository
 {
-
+    /**
+     * Gets all transactions between two dates
+     *
+     * @param \DateTime $startDate
+     * @param \DateTime $endDate
+     * @return mixed
+     */
+    public function findAllCreatedAtBetween(\DateTime $startDate, \DateTime $endDate)
+    {
+        return $this->_em->createQueryBuilder()
+            ->select('t')
+            ->from('PaymentSuite\RedsysApiBundle\Entity\Transaction', 't')
+            ->where('t.createdAt BETWEEN :startDate AND :endDate')
+           ->setParameter('startDate', $startDate->format('Y-m-d h:i:s'))
+           ->setParameter('endDate', $endDate->format('Y-m-d  h:i:s'))
+            ->getQuery()
+            ->getResult();
+    }
 }
